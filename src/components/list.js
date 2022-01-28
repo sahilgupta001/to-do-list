c3f30562c37b0c96a96dd481b1c6b49222eb6f52
Sahil Gupta <sahil.gupta@geeksforgeeks.org>
2022-01-28 16:07:16 +0530

    day-2

--- src/components/list.js
+++ src/components/list.js
@@ -5,14 +5,44 @@ export default function List() {
     
     const unCheckedListHTML = () => {
         var html = Object.keys(uncheckedList).map((uncheckedItem, index) => (
-            <p>{uncheckedList[uncheckedItem].description}</p>
+            <>
+                <div className = "col-1">
+                    <h6>{index + 1}</h6>
+                </div>
+                <div className='col-11'>
+                    <p>{uncheckedList[uncheckedItem].description}</p>
+                </div>
+            </>
         ))
         return html;
     }
     
     return (
-        <section>
-            {unCheckedListHTML()}                   
+        <section className='container mt-5'>
+            <div className = "row">
+                <div className = "col-6">
+                    <div className = "card">
+                        <div className = "card-title mt-4">
+                            <h3 className = "font-weight-bold">Tasks to be done</h3>
+                        </div>
+                        <div className = "card-body">
+                            <div className = "row">
+                                {unCheckedListHTML()}                   
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className = "col-6">
+                    <div className = "card">
+                        <div className = "card-title mt-4">
+                            <h3 className = "font-weight-bold">Tasks to be done</h3>
+                        </div>
+                        <div className = "card-body">
+                            {unCheckedListHTML()}        
+                        </div>           
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
