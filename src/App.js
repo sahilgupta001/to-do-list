c4668fc4f2dc63628f7930096c582c8426e6aef6
Sahil Gupta <sahil.gupta@geeksforgeeks.org>
2022-02-08 15:56:58 +0530

    day-7

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from './components/header'
 import Dashboard from './components/dashboard';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <Dashboard />
+      <Router>
+        <Routes>
+          <Route exact path = '/home' element = {<Dashboard />}></Route>  
+        </Routes>
+      </Router>
     </div>
   );
 }
